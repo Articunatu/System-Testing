fe0e91dbf9ab47a5ca64186681ae9410c41b8ecc
Articunatu <62301779+Articunatu@users.noreply.github.com>
2022-05-18 11:36:34 +0200

    form: Sum method from object reference

--- CalculateSum/CalculateSum_Test/AdditionTest.cs
+++ CalculateSum/CalculateSum_Test/AdditionTest.cs
@@ -1,3 +1,4 @@
+using CalculateSum;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -9,21 +10,19 @@ namespace CalculateSum_Test
         [TestMethod]
         public void Two_Odd_Sums_And_Two_Even_Sums()
         {
-            int[] terms1 = new int[] { 8, 8};
-            var sum1 = (terms1[0] + terms1[1]) % 2;
-            sum1.Should().Be(0);
+            Math m = new Math();
 
-            int[] terms2 = new int[] { 16, 8 };
-            var sum2 = (terms2[0] + terms2[1]) % 2;
-            sum2.Should().Be(0);
+            var sum1 = m.Sum(8, 8);
+            (sum1 % 2).Should().Be(0);
 
-            int[] terms3 = new int[] { 40, 5 };
-            var sum3 = (terms3[0] + terms3[1]) % 2;
-            sum3.Should().Be(1);
+            var sum2 = m.Sum(16, 8);
+            (sum2 % 2).Should().Be(0);
 
-            int[] terms4 = new int[] { 18, 1 };
-            var sum4 = (terms4[0] + terms4[1]) % 2;
-            sum4.Should().Be(1);
+            var sum3 = m.Sum(40, 5);
+            (sum3 % 2).Should().Be(1);
+
+            var sum4 = m.Sum(18, 11);
+            (sum4 % 2).Should().Be(1);
         }
     }
 }
