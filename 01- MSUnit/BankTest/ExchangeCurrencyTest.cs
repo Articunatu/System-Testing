0d2f6a3af667992e9504ae7c28db6894a41dc0fb
Articunatu <62301779+Articunatu@users.noreply.github.com>
2022-05-23 15:01:41 +0200

    fix: First part of making classes accesible

---
+++ 01- MSUnit/BankTest/ExchangeCurrencyTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using PandaBank;
+
+namespace BankTest
+{
+    public class ExchangeCurrencyTest
+    {
+        [TestMethod]
+        public void GetExchangeRate_When_SendCurrency_SEK_And_RecieveCurrency_EUR_With_Result_()
+        {
+            Admin a = new Admin();
+            decimal rate = a.ListOfCustomers[0].
+        }
+    }
+}
