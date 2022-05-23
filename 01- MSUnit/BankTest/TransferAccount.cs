c23b214f16bc3c2e65ea29149b72a2bba3321ddb
Articunatu <62301779+Articunatu@users.noreply.github.com>
2022-05-23 13:58:59 +0200

    feat: Test class for transfers

---
+++ 01- MSUnit/BankTest/TransferAccount.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BankTest
+{
+    [TestClass]
+    public class TransferAccount
+    {
+        [TestMethod]
+        public void GetMoneyTransfer_With_SendAccount_Spar_And_RecieveAccount_Lön_And_ChosenAmount_3000_With_()
+        {
+
+        }
+    }
+}
